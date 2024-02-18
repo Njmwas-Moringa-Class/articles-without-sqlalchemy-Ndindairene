35bcd1c2954e9bad53adcc640610328994dbdaa7
irene <irene.ndindaa@gmail.com>
2024-02-18 17:44:22 +0300

    done

--- lib/Author.py
+++ lib/Author.py
@@ -1,2 +1,53 @@
+from .Article import Article
 class Author:
-    pass
+    def __init__(self, name) :
+        self._name = name
+        
+    def __repr__(self) -> str:
+        return(f" {self._name}" )
+        pass
+    def name(self):
+        return self._name
+    
+    def articles(self):
+        # return a list of all articles, written by this author
+        # get a list of all articles, and filter by author
+        allArticles = Article.all_articles  # [ Author One, Tutembee,  A Visit to Lamu,  Author One, Tupike,  Tupike Chakula Kitamu,  Author One, Tupike,  Tupike Chakula Kibaya]
+        # print(f"Article.all_articles {allArticles}")
+        authorArticles = []
+        for article in allArticles:
+            if article.author() == self:
+                authorArticles.append(article)
+        # print(f" authorArticles --> {authorArticles}")
+        return [article for article in allArticles if article.author() == self]
+
+
+    def magazines(self):
+        # get a list of all magazines where the author has articles
+        # access all articles by this author
+        authorArticles = self.articles() # Articles for our Author [ Author One, Tutembee,  A Visit to Lamu,  Author One, Tupike,  Tupike Chakula Kitamu,  Author One, Tupike,  Tupike Chakula Kibaya]
+        # find out which magazines the articles belong to
+        authorMagazines = []
+        for article in authorArticles:  # [ Author One, Tutembee,  A Visit to Lamu,  Author One, Tupike,  Tupike Chakula Kitamu,  Author One, Tupike,  Tupike Chakula Kibaya]
+            authorMagazines.append(article.magazine()) if article.magazine() not in authorMagazines else ""
+        return authorMagazines  #[Tutembee, Tupike]
+    
+    def add_article(self,magazine, title):
+        #create new article instnace for this author
+        new_article = Article(self, magazine, title)
+
+    def topic_areas(self):
+        #returns a unique list containing strings
+        #this string list has categories of the magazine the author has contributed
+        unique_mag_string = self.magazines()
+
+        unique_mag_list = []
+        for mag in unique_mag_string:
+            unique_mag_list.append(mag.category)
+
+        return unique_mag_list
+
+        
+
+
+
--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,2 +1,82 @@
+from .Article import Article
 class Magazine:
-    pass
+    all_magazines = []
+    mag_articles = []
+    
+
+    def __init__(self, name, category) :
+        self._name = name
+        self._category = category
+        Magazine.all_magazines.append(self)
+        
+        
+    @property
+    #Magazine name()
+    #Returns the name of this magazine
+    def name(self):
+        return self._name
+    @property
+    #Magazine category()
+    #Returns the category of this magazine
+    def category(self):
+        return self._category
+
+    # def __repr__(self) -> str:
+    #     return (f"{self._name}" )
+        
+    @classmethod
+    def all(cls):
+        return Magazine.all_magazines
+    
+    def contributors(self):
+        allArticles = Article.all_articles
+        # get all articles for this magazine
+        margArticles = [article for article in allArticles if article.magazine() == self]
+        print(f"Checking margArticle details: {margArticles}")
+        # find authors for these articles
+        magAuthors = [article.author() for article in margArticles]
+        return magAuthors
+
+    @classmethod
+    def find_by_name(cls, name):
+        print(cls)
+        for magazine in Magazine.all_magazines:
+            if magazine.name == name:
+                return magazine
+            
+    
+    def magazine_articles(self):
+        #get list of all articles and filters out those belonging to this magazine.
+        my_articles = [article for article in Article.all_articles if article.magazine == self]
+        print(my_articles)
+        Magazine.mag_articles = my_articles
+        return my_articles
+
+
+    @classmethod
+    def article_titles(cls):
+        return [ article.title() for article in Magazine.mag_articles]
+        # pass
+        # return [article.title() for article in Article.all_articles if article.magazine() == cls.name]
+        
+        # for article in Article.all_articles:  [ Author One, Tutembee,  A Visit to Lamu,  Author One, Tupike,  Tupike Chakula Kitamu,  Author One, Tupike,  Tupike Chakula Kibaya]
+        #     if article.magazine() == cls:
+        #         return article.title
+    
+
+    def contributing_authors(self):
+        #author_list with more than two articles for the magazine
+        # print(f"mag_articles --> {Magazine.mag_articles}")
+        author_dict = {}
+        for author in self.contributors():
+            if author in author_dict:
+                author_dict[author] += 1
+
+            else:
+                author_dict[author] = 1
+
+        for name, count in author_dict.items():
+            if count >= 2:
+                return (f"{name}:{count}")
+            else:
+                ""
